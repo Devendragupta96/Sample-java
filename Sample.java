1c8c2843c3db3e87f98181c375c6ad64aa24116a
Devendragupta96 <devendragupta1302@gmail.com>
2025-01-25 14:20:39 +0530

    Second commit

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
 public class Sample {
     public static void main(String args[]){
+        int a=5;
+
+        System.out.println("Value of a "+a);
         System.out.println("Hello Class");
     }
 }
